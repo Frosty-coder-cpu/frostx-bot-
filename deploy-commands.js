6af80f3b5ed2c1d83dcb73e30fed6ef32621888e
Frost X <20pritchekaitlin@gmail.com>
2026-06-22 00:27:49 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,34 @@
+require("dotenv").config();
+const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
+const commands = [
+  new SlashCommandBuilder()
+    .setName("bug-report")
+    .setDescription("Submit a FrostX bug report")
+    .addStringOption(option =>
+      option.setName("executor")
+        .setDescription("Executor used")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName("issue")
+        .setDescription("Describe the bug")
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName("proof")
+        .setDescription("Screenshot or video link")
+        .setRequired(false)
+    )
+].map(command => command.toJSON());
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  await rest.put(
+    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    { body: commands }
+  );
+
+  console.log("FrostX commands deployed.");
+})();
